2ceeff9a99d25eb18b9dd32ab6f08dd773351ec6
ArturMingaleev <adiscov90@gmail.com>
2023-10-06 23:40:25 +0300

    task4 changed

--- src/Task4.java
+++ src/Task4.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Task4 {
+    //Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+    // Пользователю должно показаться сообщение, что пустые строки вводить нельзя
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
